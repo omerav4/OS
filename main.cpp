50df2e368bd135f4d4ff8f5c93ae76326db0aa6b
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-03-28 18:24:52 +0300

    with main file

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include "osm.cpp" // Include the header file for osm.cpp
 
 int main() {
-    double operation_time = osm_operation_time(10000000000000000000);
-//    int function_time = osm_function_time();
-//    int syscall_time = osm_syscall_time();
+    double operation_time = osm_operation_time(1000000);
+    double function_time = osm_function_time(1000000);
+    double syscall_time = osm_syscall_time(1000000);
     std::cout << "Operation time: " << operation_time << std::endl;
-//    std::cout << "Function tie: " << function_time << std::endl;
-//    std::cout << "Syscall time" << syscall_time << std::endl;
+    std::cout << "Function tie: " << function_time << std::endl;
+    std::cout << "Syscall time" << syscall_time << std::endl;
     return 0;
 }
