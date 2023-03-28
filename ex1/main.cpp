56abf747dd779600e97b48d714bc22b03456d29b
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-03-28 19:10:47 +0300

    change main

--- ex1/main.cpp
+++ ex1/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "osm.cpp" // Include the header file for osm.cpp
+#include "osm.h" // Include the header file for osm.cpp
 
 int main() {
     double operation_time = osm_operation_time(1000000);
     double function_time = osm_function_time(1000000);
     double syscall_time = osm_syscall_time(1000000);
     std::cout << "Operation time: " << operation_time << std::endl;
-    std::cout << "Function tie: " << function_time << std::endl;
+    std::cout << "Function time: " << function_time << std::endl;
     std::cout << "Syscall time" << syscall_time << std::endl;
     return 0;
 }
