87f210b4b8fb280c32409087e7b0e4e1841218f4
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-05-03 21:19:32 +0300

    more test

--- ex2-new/ex_2_tests_updated/compile_run_tests.sh
+++ ex2-new/ex_2_tests_updated/compile_run_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
-g++ -std=c++11 thread.h thread.cpp thread_scheduler.h thread_scheduler.cpp uthreads.h uthreads.cpp tests/jona1.cpp -o tests/jona1
+g++ -std=c++11 thread.cpp thread_scheduler.cpp uthreads.cpp tests/jona1.cpp -o tests/jona1
 #g++ -std=c++11 uthreads.h uthreads.cpp tests/jona2.cpp -o tests/jona2
 #g++ -std=c++11 uthreads.h uthreads.cpp tests/jona3.cpp -o tests/jona3
 #g++ -std=c++11 uthreads.h uthreads.cpp tests/jona4.cpp -o tests/jona4
 #g++ -std=c++11 uthreads.h uthreads.cpp tests/jona5.cpp -o tests/jona5
 #g++ -std=c++11 uthreads.h uthreads.cpp tests/jona6.cpp -o tests/jona6
 
-g++ -std=c++11 thread.h thread.cpp thread_scheduler.h thread_scheduler.cpp uthreads.h uthreads.cpp tests/test1.in.cpp -o tests/drive1
+#g++ -std=c++11 thread.h thread.cpp thread_scheduler.h thread_scheduler.cpp uthreads.h uthreads.cpp tests/test1.in.cpp -o tests/drive1
 #g++ -std=c++11 uthreads.h uthreads.cpp tests/test2.in.cpp -o tests/drive2
 #g++ -std=c++11 uthreads.h uthreads.cpp tests/test3.in.cpp -o tests/drive3
 #g++ -std=c++11 uthreads.h uthreads.cpp tests/test4.in.cpp -o tests/drive4
@@ -31,8 +31,8 @@ jona1
 #jona6
 #echo ""
 
-echo "Running drive1"
-drive1 &> ../output_files/drive1_res.out 
+#echo "Running drive1"
+#drive1 &> ../output_files/drive1_res.out
 #echo "Running drive2"
 #drive2 &> ../output_files/drive2_res.out
 #echo "Running drive3"
