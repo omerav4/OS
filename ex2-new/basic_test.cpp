7043368ff01b25a773f1870aa9bf3995ebbdf9e5
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-05-03 19:41:27 +0300

    ex2-new

--- ex2-new/basic_test.cpp
+++ ex2-new/basic_test.cpp
@@ -703,7 +703,7 @@ int main()
 //    check_swap();
 //    basic_sleep_check();
 //    basic_block_check();
-    get_limit_error();
+//    get_limit_error();
     check_uthread_get_quantums();
     check_uthread_terminate();
     test_deleting_threads();
