c625664af97f5db531e8dc02978b966813993e4d
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-05-26 08:41:41 +0300

    ex3 new file

--- ex3-new/cmake-build-debug/mattanTests/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cs/usr/inbar.oren/CLionProjects/ex3New/cmake-build-debug/mattanTests/googletest-src/googletest
-# Build directory: /cs/usr/inbar.oren/CLionProjects/ex3New/cmake-build-debug/mattanTests/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
