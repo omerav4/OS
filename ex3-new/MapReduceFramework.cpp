cb9b649227d58c04170d8e7163313c6552cdd06a
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-05-26 10:55:52 +0300

    ex3 new testsss

--- ex3-new/MapReduceFramework.cpp
+++ ex3-new/MapReduceFramework.cpp
@@ -15,7 +15,7 @@
 
 #define ERROR_MESSAGE_ALLOCATION_FAILURE "system error: allocation failure\n"
 #define ERROR_MESSAGE_PTHREAD_FAILURE "system error: pthread creation failed\n"
-#define JOIN_FAILURE "system error: pthread join  failed\n"
+#define JOIN_FAILURE "system error: pthread join  failedddd\n"
 #define MUTEX_UNLOCK_FAILURE "system error: mutex unlock  failed\n"
 #define MUTEX_LOCK_FAILURE "system error: mutex lock  failed\n"
 #define MUTEX_DESTROY_ERR "system error: [[Barrier]] error on pthread_mutex_destroy"
