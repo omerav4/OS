c43c5ad3af3b154af5995c1fe87caefa09626e0b
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-06-20 18:22:42 +0300

    ex4 - debug

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -104,9 +104,9 @@ void transverse_tree(page* node, uint64_t cur_level, int cur_row, uint64_t* max_
     // in addition, we will update the max_frame_index to keep the maximum frame index
     bool is_empty = true;
     for (uint64_t row = 0; row < PAGE_SIZE; ++row){   // recursive call
+        printf("row %llu\n", row);
         initialize_next_node(node);
         PMread(node->address * PAGE_SIZE + row, &(node->next->address));
-        printf("row %llu\n", row);
         printf("next->address before %d\n", node->next->address);
 
         if (node->next->address != 0) {  // page is full, continue searching in next level
