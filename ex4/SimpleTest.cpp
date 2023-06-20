fa6cc3d0b40df5558606c37d09f7a53fa7477e72
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-06-20 23:44:10 +0300

    ex4 - debug

--- ex4/SimpleTest.cpp
+++ ex4/SimpleTest.cpp
@@ -1,31 +1,21 @@
 #include "VirtualMemory.h"
-#include "PhysicalMemory.h"
 
 #include <cstdio>
 #include <cassert>
 
 int main(int argc, char **argv) {
-    int value;
     VMinitialize();
-    VMwrite(13,3);
+    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+        printf("writing to %llu\n", (long long int) i);
+        VMwrite(5 * i * PAGE_SIZE, i);
+    }
 
-    VMread(6,&value);
-
-    printf("--------------------------------\n");
-    VMread(31, &value);
-    printf("value %d\n", value);
-
-//    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
-//        printf("writing to %llu\n", (long long int) i);
-//        VMwrite(5 * i * PAGE_SIZE, i);
-//    }
-//
-//    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
-//        word_t value;
-//        VMread(5 * i * PAGE_SIZE, &value);
-//        printf("reading from %llu %d\n", (long long int) i, value);
-//        assert(uint64_t(value) == i);
-//    }
+    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+        word_t value;
+        VMread(5 * i * PAGE_SIZE, &value);
+        printf("reading from %llu %d\n", (long long int) i, value);
+        assert(uint64_t(value) == i);
+    }
     printf("success\n");
 
     return 0;
