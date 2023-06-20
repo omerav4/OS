85e7b3b343aa44279782617cde0c24dd4a7a8158
omerav4 <101065282+omerav4@users.noreply.github.com>
2023-06-20 18:25:55 +0300

    ex4 - debug

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -103,8 +103,7 @@ void transverse_tree(page* node, uint64_t cur_level, int cur_row, uint64_t* max_
     // if a child exists (means there is row != 0), then the current frame is not empty
     // in addition, we will update the max_frame_index to keep the maximum frame index
     bool is_empty = true;
-    for (uint64_t row = 0; row < PAGE_SIZE; ++row){   // recursive call
-        printf("hellooo\n");
+    for (uint64_t row = 0; row < PAGE_SIZE; row++){   // recursive call
         printf("row %llu\n", row);
         initialize_next_node(node);
         PMread(node->address * PAGE_SIZE + row, &(node->next->address));
